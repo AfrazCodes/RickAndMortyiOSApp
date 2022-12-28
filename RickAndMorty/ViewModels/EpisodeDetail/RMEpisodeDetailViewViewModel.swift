693027bfea509b81b8f55bbe229021c94cc80296
peak <peak@jiemogame.com>
2022-12-28 15:00:30 +0800

    add async feature: use task group to replace dispatch group when get episode detail info

--- RickAndMorty/ViewModels/EpisodeDetail/RMEpisodeDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/EpisodeDetail/RMEpisodeDetailViewViewModel.swift
@@ -83,8 +83,9 @@ final class RMEpisodeDetailViewViewModel {
         Task {
             let result = await RMService.shared.execute(request, excepting: RMEpisode.self)
             switch result {
-            case .success(let model):
-                self.fetchRelatedCharacters(episode: model)
+            case .success(let episode):
+//                self.fetchRelatedCharacters(episode: episode)
+                await asyncFetchRelatedCharacters(episode: episode)
             case .failure:
                 break
             }
@@ -126,4 +127,49 @@ final class RMEpisodeDetailViewViewModel {
             )
         }
     }
+    
+    private func asyncFetchRelatedCharacters(episode: RMEpisode) async {
+        let requests: [RMRequest] = episode.characters.compactMap({
+            return URL(string: $0)
+        }).compactMap({
+            return RMRequest(url: $0)
+        })
+
+        let characters = await withTaskGroup(of: RMCharacter?.self, returning: [RMCharacter].self, body: { taskGroup in
+            print("Task start   : \(Date())")
+            
+            var childResult: [RMCharacter] = []
+            for request in requests {
+                // run chil task
+                taskGroup.addTask {
+                    let result = await RMService.shared.execute(request, excepting: RMCharacter.self)
+                    switch result {
+                    case .success(let model):
+                        return model
+                    case .failure(let error):
+                        print("get character occur error: \(error)")
+                        return nil
+                    }
+                }
+                // collect the child task result
+                for await taskResult in taskGroup {
+                    if let result = taskResult {
+                        childResult.append(result)
+                    }
+                }
+            }
+            return childResult
+        })
+      
+        print("Task end     : \(Date())")
+        print("allResults   : \(characters.count)")
+        
+        await MainActor.run(body: {
+            self.dataTuple = (
+                episode: episode,
+                characters: characters
+            )
+        })
+    }
+    
 }
